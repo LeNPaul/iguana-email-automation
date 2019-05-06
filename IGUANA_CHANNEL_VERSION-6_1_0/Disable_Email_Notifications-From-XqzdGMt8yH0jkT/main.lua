6076d95594ab60614b129ba3105688066c2dcce1
admin <solutions@interfaceware.com>
2019-05-06 17:25:51 +0000

    Export: Export most recent channels.

--- IGUANA_CHANNEL_VERSION-6_1_0/Disable_Email_Notifications-From-XqzdGMt8yH0jkT/main.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/Disable_Email_Notifications-From-XqzdGMt8yH0jkT/main.lua
@@ -4,32 +4,40 @@ local scheduler = {}
 scheduler.runAt = require 'scheduler'
 
 function main()
-   
+
    iguana.setTimeout(300)
 
    local weekday = os.date('%A')
    local date = os.date('%x')
+   local hours = os.date('%H')
    local minutes = os.date('%M')
    local seconds = os.date('%S')
 
    local holidays = {
-      '07/01/18',
-      '08/06/18',
-      '09/03/18',
-      '10/08/18',
-      '12/25/18',
-      '12/26/18'
+      '01/01/19',
+      '02/18/19',
+      '04/19/19',
+      '05/20/19',
+      '07/01/19',
+      '09/02/19',
+      '10/14/19',
+      '12/25/19',
+      '12/26/19'
    }
---[[
+
+   --[[ The Enable Email Notifications channel is already pinging
    if minutes:sub(2,2) == '0' and seconds:sub(1,1) == '0' then 
-      retry.call{func=autoresponder.ping, arg1=callUrl,retry=4, pause=45}
-   end
---]]
-   if weekday ~= 'Saturday' and weekday ~= 'Sunday' then
+   retry.call{func=autoresponder.ping, arg1=callUrl,retry=4, pause=45}
+end
+   --]]
+
+   if weekday ~= 'Saturday' and weekday ~= 'Sunday' and hours == '09' then
 
       if date ~= holidays[1] and date ~= holidays[2] and 
          date ~= holidays[3] and date ~= holidays[4] and 
-         date ~= holidays[5] and date ~= holidays[6] then 
+         date ~= holidays[5] and date ~= holidays[6] and
+         date ~= holidays[7] and date ~= holidays[8] and
+         date ~= holidays[9] then 
 
          -- Disable email automation
          scheduler.runAt(9, autoresponder.disable)
